27bfafea17848f6b2c12abaf97302fc1eaa58323
Brian <brianos-17git@hotmail.com>
2017-10-18 17:35:58 +0100

    Created TimelineItem adapter to create and update timeline tweets as they are created

---
+++ app/src/main/java/org/wit/mytweet/adapters/TimelineItem.java
@@ -0,0 +1,27 @@
+package org.wit.mytweet.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.wit.mytweet.R;
+import org.wit.mytweet.models.Tweet;
+
+public class TimelineItem {
+    View view;
+
+    public TimelineItem(Context context, ViewGroup parent, OnClickListener deleteListener, Tweet tweet) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = inflater.inflate(R.layout.content_timeline_item, parent, false);
+        view.setId(tweet.tweetId);
+        updateTimeline(tweet);
+    }
+
+    private void updateTimeline(Tweet tweet) {
+        ((TextView) view.findViewById(R.id.tweetSubstring)).setText(tweet.message.substring(0,20));
+        ((TextView) view.findViewById(R.id.tweetDate)).setText(tweet.date);
+    }
+}
--- app/src/main/java/org/wit/mytweet/models/Tweet.java
+++ app/src/main/java/org/wit/mytweet/models/Tweet.java
@@ -7,9 +7,12 @@ public class Tweet implements Serializable {
 
     public String message;
     public String date;
+    public static int autoId = 1;
+    public int tweetId;
 
     public Tweet(String message, String date) {
         this.message = message;
         this.date = date;
+        this.tweetId = autoId++;
     }
 }
