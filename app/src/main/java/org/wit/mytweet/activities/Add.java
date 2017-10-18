2c67cad92171f308017af2f96a21f247fa0978de
Brian <brianos-17git@hotmail.com>
2017-10-18 16:02:23 +0100

    added code to set characterCount and count this number down in activity_add

--- app/src/main/java/org/wit/mytweet/activities/Add.java
+++ app/src/main/java/org/wit/mytweet/activities/Add.java
@@ -3,6 +3,9 @@ package org.wit.mytweet.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.wit.mytweet.R;
@@ -12,14 +15,39 @@ import java.util.Date;
 
 public class Add extends AppCompatActivity {
 
+    private TextView characterCount;
+    private EditText newTweet;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
         TextView tweetDate = (TextView) findViewById(R.id.tweetDate);
+        characterCount = (TextView) findViewById(R.id.characterCount);
+        characterCount.setText(String.valueOf(140));//Sets value of TextView to 140
+        newTweet = (EditText) findViewById(R.id.newTweet);
         //Code to get today's date in form of string and pass to tweetDate TextView
+        //Retrieved from: https://stackoverflow.com/questions/2271131/display-the-current-time-and-date-in-an-android-application
         String currentDate = DateFormat.getDateTimeInstance().format(new Date());
         tweetDate.setText(currentDate);
+        //TextWatcher which counts down value of character count
+        //Retrieved from: https://stackoverflow.com/questions/24110265/android-create-count-down-word-field-when-user-type-in-edittext
+        newTweet.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//                characterCount.setText(String.valueOf(140));
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                characterCount.setText(String.valueOf(140 - (newTweet.getText().toString().length())));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
--- app/src/main/java/org/wit/mytweet/activities/SignUp.java
+++ app/src/main/java/org/wit/mytweet/activities/SignUp.java
@@ -21,7 +21,6 @@ public class SignUp extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-
         registerButton = (Button) findViewById(R.id.registerButton);
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
