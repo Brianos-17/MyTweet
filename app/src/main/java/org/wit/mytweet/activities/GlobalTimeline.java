e53d05866b1e67cec6d6fd75f3598218210478db
Brian <brianos-17git@hotmail.com>
2017-10-30 20:38:28 +0000

    Added GlobalTimeline activity and layout, added this to Manifest file

---
+++ app/src/main/java/org/wit/mytweet/activities/GlobalTimeline.java
@@ -0,0 +1,16 @@
+package org.wit.mytweet.activities;
+
+import android.os.Bundle;
+
+import org.wit.mytweet.R;
+
+/**
+ * Created by Brian on 30/10/2017.
+ */
+
+public class GlobalTimeline extends Base {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_global_timeline);
+}
