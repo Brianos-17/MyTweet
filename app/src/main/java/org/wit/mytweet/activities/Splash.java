cec9f6251dc84a109268a58db957bbbd289eb16d
Brian <brianos-17git@hotmail.com>
2017-12-30 10:22:52 +0000

    Refactored Splash activity to display ImageView

--- app/src/main/java/org/wit/mytweet/activities/Splash.java
+++ app/src/main/java/org/wit/mytweet/activities/Splash.java
@@ -5,11 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import org.wit.mytweet.R;
 
 
-public class Splash extends Activity {
+public class Splash extends AppCompatActivity {
 
     private static final int 	SPLASH_DURATION = 5000; // 2 seconds
     private boolean backButtonPressed;
@@ -18,6 +19,7 @@ public class Splash extends Activity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ImageView splashImage = (ImageView) findViewById(R.id.splashImage);
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
